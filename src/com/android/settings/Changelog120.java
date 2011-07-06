811985285c9392aca67adab0a6bccc7a00ba13d3
r2DoesInc <r2doesinc@gmail.com>
2011-07-06 02:01:01 -0400

    OMFGB >> OMGB branding

--- src/com/android/settings/Changelog120.java
+++ src/com/android/settings/Changelog120.java
@@ -9,7 +9,7 @@ public class Changelog120 extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       tv.setText("-Rebuilt from scratch yet again." + "\n-Updated to android 2.3.3." + "\n-New Lockscreen music controls." + "\n-New features for the toggle widgets from cm." + "\n-Integration of the theme engine from Tmobile." + "\n-About OMFGB mod added to Settings menu." + "\n-Wimax enabled for applicable devices"+ "\n-Add Eris to list of supported devices.");
+       tv.setText("-Rebuilt from scratch yet again." + "\n-Updated to android 2.3.3." + "\n-New Lockscreen music controls." + "\n-New features for the toggle widgets from cm." + "\n-Integration of the theme engine from Tmobile." + "\n-About OMGB mod added to Settings menu." + "\n-Wimax enabled for applicable devices"+ "\n-Add Eris to list of supported devices.");
        setContentView(tv);
    }
 }
