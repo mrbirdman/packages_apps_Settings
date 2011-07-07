d70b422b4fdbd27c1a20b1e05a33777c0a1d76d1
r2DoesInc <r2doesinc@gmail.com>
2011-07-07 00:57:39 -0400

    Update changelog

--- src/com/android/settings/Changelog120.java
+++ src/com/android/settings/Changelog120.java
@@ -9,7 +9,7 @@ public class Changelog120 extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       tv.setText("-Rebuilt from scratch yet again.");
+       tv.setText("-Rebuilt from scratch yet again." + "\n-Update to Android 2.3.4" + "\n-Add support for OG Droid, CDMA Hero, and the Thunderbolt." + "\n-Add Tmobile Theme Engine." + "\n-Add FM Radio for applicable devices.");
        setContentView(tv);
    }
 }
