f6c93d58cea91113c7eb250a6c02c9e87725e629
r2DoesInc <r2doesinc@gmail.com>
2011-07-21 02:39:49 -0400

    Update changelog

--- src/com/android/settings/Changelog120.java
+++ src/com/android/settings/Changelog120.java
@@ -9,7 +9,7 @@ public class Changelog120 extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       tv.setText("-Rebuilt from scratch yet again." + "\n-Update to Android 2.3.4" + "\n-Add support for OG Droid, CDMA Hero, and the Thunderbolt." + "\n-Add Tmobile Theme Engine." + "\n-Add FM Radio for applicable devices.");
+       tv.setText("-Rebuilt from scratch yet again." + "\n-Update to Android 2.3.4" + "\n-Inclusion of stripped down God Mode for easy access to Addons and Nightlies." + "\n-Inclusion of T3hh4xx0r Wallpapers." + "\n-Add support for Fascinate, OG Droid, CDMA Hero, Incredible 2, and the Thunderbolt." + "\n-Add Tmobile Theme Engine." + "\n-Add FM Radio for applicable devices.");
        setContentView(tv);
    }
 }
